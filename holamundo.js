a01600fc60abe2dae40d1b9aa972c3bab08adc5a
o <E301@Academico.local>
2023-06-21 19:24:02 -0500

    Test 2

--- holamundo.js
+++ holamundo.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const holamundo = express()
 
-holamundo.get('/', (req, res) => res.send('This a test for CI/CD'))
+holamundo.get('/', (req, res) => res.send('This a second test for CI/CD'))
 
 module.exports = holamundo
