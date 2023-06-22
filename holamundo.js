8617b7420cff294e154429b90b1aac89c3ab2b2c
Jole24 <jolerep@gmail.com>
2023-06-21 19:44:31 -0500

    testing branches

--- holamundo.js
+++ holamundo.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const holamundo = express()
+const suma = express()
 
 holamundo.get('/', (req, res) => res.send('This a test for CI/CD'))
+
+suma.get('/', (req, res) => res.send(6 + 6))
 
 module.exports = holamundo
