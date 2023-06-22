be127915d131589f093909af281f4b212ed19263
E301 sede B Tecnologico Comfenalco <E301@Academico.local>
2023-06-21 20:56:25 -0500

    agregando rama de multiplicacion

--- holamundo.js
+++ holamundo.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const holamundo = express()
 
-holamundo.get('/', (req, res) => res.send('This a second test for CI/CD'))
+holamundo.get('/', (req, res) => {
+    const result = 56*2;
+    res.send("El resultado de la multiplicacion es: " + result)
 
+});
 module.exports = holamundo
