924ba7fc6a0cfa3239422a95c530d868a998bfe8
E301 sede B Tecnologico Comfenalco <E301@Academico.local>
2023-06-21 20:53:42 -0500

    SumaYResta

--- holamundo.js
+++ holamundo.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const holamundo = express()
 
-holamundo.get('/', (req, res) => res.send('This a second test for CI/CD'))
+holamundo.get('/', (req, res) => {
+        const result= 59+20-10
+        res.send("Resultado:  " + result) } ); 
+
+
 
 module.exports = holamundo
