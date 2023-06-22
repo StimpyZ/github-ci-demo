0674c23df2502b1d955951aa94cdc32d89135b2a
Jole24 <jolerep@gmail.com>
2023-06-21 19:07:20 -0500

    adding new message

--- holamundo.js
+++ holamundo.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const holamundo = express()
 
-holamundo.get('/', (req, res) => res.send('Hola Mundo!'))
+holamundo.get('/', (req, res) => res.send('This a test for CI/CD'))
 
 module.exports = holamundo
