ccb68a0ff77f278f5979bf997a32bb8ff085cd15
o <E301@Academico.local>
2023-06-21 20:28:02 -0500

    division

--- holamundo.js
+++ holamundo.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const holamundo = express()
 
-holamundo.get('/', (req, res) => res.send('This a second test for CI/CD'))
+holamundo.get('/', (req, res) => {
+    const result = 10/2;
+    res.send("El resultado de la division " + result)
+
+
+
+
+});
 
 module.exports = holamundo
