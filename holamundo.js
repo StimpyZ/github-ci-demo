ae0820cec2a250f500660788a4ee8417e4264160
E301 sede B Tecnologico Comfenalco <E301@Academico.local>
2023-06-21 21:13:08 -0500

    rama de elevacion

--- holamundo.js
+++ holamundo.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const holamundo = express()
 
-holamundo.get('/', (req, res) => res.send('This a second test for CI/CD'))
-
+holamundo.get('/', (req, res) => {
+    const a = 318;
+    const result = 318**2
+    res.send(`El resultado ${a} elevado a la 2 es: ${result}`)
+});
 module.exports = holamundo
